e560bc41bece4c21b3df49fffaf921a4ea2e5569
OdinRossy <remniov.work@gmail.com>
2019-11-20 20:16:29 +0300

    Updates

---
+++ precache-manifest.1e10d22aa7bedddd777f1ccf39f64907.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dff100d1923c77e1f1849e2158bb086",
+    "url": "/f-auto-course-project/index.html"
+  },
+  {
+    "revision": "cd1704802e8ccb19fb61",
+    "url": "/f-auto-course-project/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dbe6573b22cf995ec1cb",
+    "url": "/f-auto-course-project/static/js/2.15b7f8a7.chunk.js"
+  },
+  {
+    "revision": "cd1704802e8ccb19fb61",
+    "url": "/f-auto-course-project/static/js/main.e35a9db1.chunk.js"
+  },
+  {
+    "revision": "855441f2e542762e41c1",
+    "url": "/f-auto-course-project/static/js/runtime-main.c372d692.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/f-auto-course-project/static/media/logo.25bf045c.svg"
+  }
+]);
